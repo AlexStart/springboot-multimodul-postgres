bf8061ca4ad074c0066613485d1d7dcff5d37fb9
Aliaksandr Apanovich <gladivs@sam-solutions.net>
2017-05-26 18:12:16 +0300

    some changes

--- application/src/main/java/my/springboot/multimodul/postgres/configuration/DbMigrationConfig.java
+++ application/src/main/java/my/springboot/multimodul/postgres/configuration/DbMigrationConfig.java
@@ -7,6 +7,7 @@ import org.springframework.boot.autoconfigure.flyway.FlywayDataSource;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceBuilder;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Primary;
 import org.springframework.context.annotation.Profile;
 import org.springframework.context.support.PropertySourcesPlaceholderConfigurer;
 
@@ -41,7 +42,8 @@ public class DbMigrationConfig {
     }
 
     @Bean
-    @FlywayDataSource
+    @Primary
+    //@FlywayDataSource
     public DataSource dataSource() {
         System.out.println("@@@@@@@@@@@@@@ "+DB_URL+" @@@@@@@@@@@@@@@");
         System.out.println("@@@@@@@@@@@@@@ "+DB_USERNAME+" @@@@@@@@@@@@@@@");
